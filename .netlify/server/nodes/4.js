c942d0a3d7526f62d03106720ed13a9b4b0dacc4
Eric D. Schmid <117695342+schmidward@users.noreply.github.com>
2023-07-04 14:12:50 -0500

    some updates

--- .netlify/server/nodes/4.js
+++ .netlify/server/nodes/4.js
@@ -2,6 +2,6 @@
 
 export const index = 4;
 export const component = async () => (await import('../entries/pages/mywork/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/4.2bdf8dfb.js","_app/immutable/chunks/index.f779f945.js"];
+export const imports = ["_app/immutable/nodes/4.2bba3266.js","_app/immutable/chunks/index.f779f945.js"];
 export const stylesheets = [];
 export const fonts = [];
