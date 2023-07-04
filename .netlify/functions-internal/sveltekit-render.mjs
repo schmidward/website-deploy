b470af29d7fada00b9195959a4cb36fba08de200
Eric D. Schmid <117695342+schmidward@users.noreply.github.com>
2023-07-04 13:56:03 -0500

    Structural work

---
+++ .netlify/functions-internal/sveltekit-render.mjs
@@ -0,0 +1,61 @@
+import { init } from '../serverless.js';
+
+export const handler = init({
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		client: {"start":"_app/immutable/entry/start.bd9cc7d8.js","app":"_app/immutable/entry/app.4acd4bb8.js","imports":["_app/immutable/entry/start.bd9cc7d8.js","_app/immutable/chunks/index.f779f945.js","_app/immutable/chunks/singletons.5f6f5dea.js","_app/immutable/entry/app.4acd4bb8.js","_app/immutable/chunks/index.f779f945.js"],"stylesheets":[],"fonts":[]},
+		nodes: [
+			() => import('../server/nodes/0.js'),
+			() => import('../server/nodes/1.js'),
+			() => import('../server/nodes/2.js'),
+			() => import('../server/nodes/3.js'),
+			() => import('../server/nodes/4.js'),
+			() => import('../server/nodes/5.js'),
+			() => import('../server/nodes/6.js')
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			},
+			{
+				id: "/about",
+				pattern: /^\/about\/?$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 3 },
+				endpoint: null
+			},
+			{
+				id: "/mywork",
+				pattern: /^\/mywork\/?$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 4 },
+				endpoint: null
+			},
+			{
+				id: "/mywork/2023/riverfront-developments",
+				pattern: /^\/mywork\/2023\/riverfront-developments\/?$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 5 },
+				endpoint: null
+			},
+			{
+				id: "/resume",
+				pattern: /^\/resume\/?$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 6 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+});
---
+++ .netlify/server/nodes/1.js
@@ -0,0 +1,7 @@
+
+
+export const index = 1;
+export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/1.49bf84e3.js","_app/immutable/chunks/index.f779f945.js","_app/immutable/chunks/singletons.5f6f5dea.js"];
+export const stylesheets = [];
+export const fonts = [];
