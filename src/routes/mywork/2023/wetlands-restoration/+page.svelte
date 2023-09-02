e208ffc6e87747f32bc7c38cf36a876432b32aef
Eric D. Schmid <117695342+schmidward@users.noreply.github.com>
2023-09-02 18:43:00 -0500

    updates to article template page.

--- src/routes/mywork/2023/wetlands-restoration/+page.svelte
+++ src/routes/mywork/2023/wetlands-restoration/+page.svelte
@@ -10,20 +10,24 @@
                     <p>Wetlands restoration is key to avoiding the worst of future floods along the Mississippi</p>
                 </div>
                 <div class="self-start mt-4">
-                    <p>Published by: <span class="text-dark-red hover:text-hover-red"><a href="https://www.marketplace.org/2023/06/20/as-mississippi-riverfront-development-booms-wetlands-become-crucial-to-stave-off-flooding/">Marketplace</a></span> & <span class="text-dark-red hover:text-hover-red"><a href="https://news.stlpublicradio.org/health-science-environment/2023-06-29/wetlands-restoration-is-key-to-avoiding-the-worst-of-future-floods-along-the-mississippi">St. Louis Public Radio</a></span></p>
+                    <p>Published by: 
+                        <span class="text-dark-red hover:text-hover-red"><a href="https://www.marketplace.org/2023/06/20/as-mississippi-riverfront-development-booms-wetlands-become-crucial-to-stave-off-flooding/">
+                            Marketplace</a></span> & 
+                        <span class="text-dark-red hover:text-hover-red"><a href="https://news.stlpublicradio.org/health-science-environment/2023-06-29/wetlands-restoration-is-key-to-avoiding-the-worst-of-future-floods-along-the-mississippi">
+                            St. Louis Public Radio</a></span>
+                        </p>
                 </div>
             </div>
-            <div class="">
+            <div class="font-extralight text-sm">
                 <img 
                     src= "../../../images/2023-05-24_ES_Mike-Sertle_Lower-End-2.jpg" 
                     alt="The Mississippi River"/>
-                <p>The Old Chain of Rocks Bridge is seen in the distance last week from the middle of the Mississippi River. 
-                    Over the years, developers have been challenged in trying to tame the mighty river's shores for the sake 
-                    of economic development. CREDIT: <em>BRIAN MUÑOZ / ST. LOUIS PUBLIC RADIO</em></p>
+                <p>Mike Sertle indicates how high the water can get in a roughly 250-acre restored wetland behind him on Horseshoe Lake in Illinois 
+                    on May 24, 2023. Sertle, who manages wetlands restoration projects on the Mississippi River for Ducks Unlimited, explained that 
+                    these projects help store excess water from river flooding and heavy rains. CREDIT: <em>ERIC SCHMID / ST. LOUIS PUBLIC RADIO</em></p>
             </div>
             <article>
-                <p class="">This article was originally <a href="https://news.stlpublicradio.org/economy-business/2023-02-14/riverfront-developments-face-obsLtacles-but-the-mississippi-river-itself-may-be-the-biggest">published by St. Louis Public Radio</a> on February 14th, 2023.</p>
-                
+                                
             </article>
         </div>  
     </div>
