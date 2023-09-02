caf1dd36393fd4354979c971235615ac6be95b2e
Eric D. Schmid <117695342+schmidward@users.noreply.github.com>
2023-09-02 18:27:17 -0500

    messing around with article template

--- src/routes/mywork/2023/riverfront-developments/+page.svelte
+++ src/routes/mywork/2023/riverfront-developments/+page.svelte
@@ -3,47 +3,26 @@
 </script>
 
 <section>
-    <div class="main-content-wrapper">
-        <div class="hed">
-            <h1>Riverfront developments face obstacles, but the Mississippi River itself may be the biggest</h1>
+    <div class="max-w-screen-lg block mr-auto ml-auto">
+        <div class="flex flex-col items-center mt-6 mb-6">
+            <div class="font-semibold text-3xl">
+                <p>Wetlands restoration is key to avoiding the worst of future floods along the Mississippi</p>
+            </div>
+            <div>
+                <p>This article was originally <a href="https://news.stlpublicradio.org/economy-business/2023-02-14/riverfront-developments-face-obstacles-but-the-mississippi-river-itself-may-be-the-biggest">published by Marketplace</a> on June 20, 2023</p>
+            </div>
         </div>
-        <div class="img-and-caption">
-            <img src="../../src/lib/images/2023-02-06_BM_RIVER-26.jpeg" alt="The Mississippi River"/>
+        <div class="">
+            <img 
+                src= "../../../images/2023-05-24_ES_Mike-Sertle_Lower-End-2.jpg" 
+                alt="The Mississippi River"/>
             <p>The Old Chain of Rocks Bridge is seen in the distance last week from the middle of the Mississippi River. 
                 Over the years, developers have been challenged in trying to tame the mighty river's shores for the sake 
                 of economic development. CREDIT: <em>BRIAN MUÑOZ / ST. LOUIS PUBLIC RADIO</em></p>
         </div>
         <article>
-            <p class="pub">This article was originally <a href="https://news.stlpublicradio.org/economy-business/2023-02-14/riverfront-developments-face-obstacles-but-the-mississippi-river-itself-may-be-the-biggest">published by St. Louis Public Radio</a> on February 14th, 2023.</p>
+            <p class="pub">This article was originally <a href="https://news.stlpublicradio.org/economy-business/2023-02-14/riverfront-developments-face-obsLtacles-but-the-mississippi-river-itself-may-be-the-biggest">published by St. Louis Public Radio</a> on February 14th, 2023.</p>
             
         </article>
     </div>
 </section> 
-
-<style>
-.main-content-wrapper {
-    max-width: 992px;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-}
-
-.hed h1 {
-    font-size: 2.25em;
-    margin-top: 1%;
-    margin-bottom: 1%;
-}
-
-.pub {
-    text-align: center;
-}
-
-.img-and-caption p {
-    margin: 0;
-    font-size: 12px;
-}
-
-img {
-    width: 100%;
-}
-</style>
