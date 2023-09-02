86eea1fc1a939ec2226d366dc0d2628b303856b8
Eric D. Schmid <117695342+schmidward@users.noreply.github.com>
2023-09-02 18:38:06 -0500

    more updates to article template page.

---
+++ src/routes/mywork/2023/wetlands-restoration/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+
+</script>
+
+<section>
+    <div class="max-w-screen-lg block lg:mr-auto lg:ml-auto">
+        <div class="m-5">
+            <div class="flex flex-col items-center mt-4 mb-4">
+                <div class="font-semibold text-3xl">
+                    <p>Wetlands restoration is key to avoiding the worst of future floods along the Mississippi</p>
+                </div>
+                <div class="self-start mt-4">
+                    <p>Published by: <span class="text-dark-red hover:text-hover-red"><a href="https://www.marketplace.org/2023/06/20/as-mississippi-riverfront-development-booms-wetlands-become-crucial-to-stave-off-flooding/">Marketplace</a></span> & <span class="text-dark-red hover:text-hover-red"><a href="https://news.stlpublicradio.org/health-science-environment/2023-06-29/wetlands-restoration-is-key-to-avoiding-the-worst-of-future-floods-along-the-mississippi">St. Louis Public Radio</a></span></p>
+                </div>
+            </div>
+            <div class="">
+                <img 
+                    src= "../../../images/2023-05-24_ES_Mike-Sertle_Lower-End-2.jpg" 
+                    alt="The Mississippi River"/>
+                <p>The Old Chain of Rocks Bridge is seen in the distance last week from the middle of the Mississippi River. 
+                    Over the years, developers have been challenged in trying to tame the mighty river's shores for the sake 
+                    of economic development. CREDIT: <em>BRIAN MUÑOZ / ST. LOUIS PUBLIC RADIO</em></p>
+            </div>
+            <article>
+                <p class="">This article was originally <a href="https://news.stlpublicradio.org/economy-business/2023-02-14/riverfront-developments-face-obsLtacles-but-the-mississippi-river-itself-may-be-the-biggest">published by St. Louis Public Radio</a> on February 14th, 2023.</p>
+                
+            </article>
+        </div>  
+    </div>
+</section> 
