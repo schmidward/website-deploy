bd547f5f35e491ad072f385765b7bafb8efd9566
Eric D. Schmid <117695342+schmidward@users.noreply.github.com>
2024-11-02 13:52:06 -0400

    added speaking page

--- src/routes/speaking/+page.svelte
+++ src/routes/speaking/+page.svelte
@@ -12,26 +12,25 @@
 <section>
     <div class="max-w-screen-lg block mr-auto ml-auto mt-14 mb-14">
         <div class="flex flex-col m-5 md:flex-row">
-            <div class="flex flex-col">
-                <img src="./images/headshot-crop.jpg" alt="Eric Schmid" />
+            <div class="md:ml-10">
+                <p class="">Eric Schmid is available to book for a variety of engagements with experience moderating live panels and other discussions, leading presentations, facilitating trainings, and as a keynote speaker. He  understands how the style of presentation is just as important as the content when crafting engaging and memorable professional experiences.</p> 
+
+                <p class="mt-7">Eric has expertise with the journalism and media industry and the topics he regularly covers as a professional reporter like economics, business development, the environment & climate, and the Mississippi River basin. He is also comfortable leading journalism specific trainings on many aspects of broadcast radio and podcasting, including story development, writing, production techniques, and others.</p>
+
+                <p class="mt-7">Have an event you want Eric for? Use the button to start an email inquiry. Please include the following in your message: </p>
+                <ul class="ml-6">
+                    <li>- The date, time and event location</li>
+                    <li>- A short event description</li>
+                    <li>- Anticipated audience demographics (professionals, students, etc.)</li>
+                    <li>- What you wat to stick with attendees afterward</li>
+                </ul>
+                <p class="mt-7">These don’t need to fully fleshed out and are more intended to help start the conversation with Eric to help him plan a memorable and impactful event for you.</p>
                 <div class="flex justify-center mr-5 ml-5">
                     <a  class="bg-red text-white mt-6 mb-3 p-5 text-center hover:bg-hover-red" 
-                    href="mailto:eschmid@stlpr.org">
-                    Contact Me
+                    href="mailto:eschmid@stlpr.org?subject=Speaking inquiry">
+                    Book Eric
                     </a>
                 </div>
-                
-            </div>
-            <div class="md:ml-10">
-                <p class="">I am originally from of Fort Collins, Colorado but found my way to the Midwest, after a brief four-year stop in New York where I graduated from Stony Brook University in 2018.</p> 
-
-                <p class="mt-7">In 2019, I landed in the St. Louis region—Belleville, IL specifically—to establish St. Louis Public Radio’s coverage of the Metro East. I since moved into the City of St. Louis and in 2022 was promoted to cover economic development across the region’s 2.8 million people.</p>
-
-                <p class="mt-7">Outside of work, I enjoy the outdoors, and it’s where I like to be when not on assignment. I love everything from a float trip, to finding new hiking trails or just walking through one of St. Louis’ many green spaces.</p>
-
-                <p class="mt-7">I also gravitate to landscape and nature photography and love the bluffs of the rivers here and the big sky country near Laramie, Wyoming. A major life goal of mine is to photograph the aurora borealis.</p>
-
-                <p class="mt-7">Music is another pillar for me. I grew up listening to jazz and classical and have been playing instruments almost all my life. These days, I play the tenor saxophone and the clarinet.</p>
             </div>
         </div>
     </div>
